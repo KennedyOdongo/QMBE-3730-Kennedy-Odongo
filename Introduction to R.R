052a9a9bd938e0a2c6f3963e78ffff0ff981de98
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-12 08:52:27 -0600

    added R code

---
+++ Introduction to R.R
@@ -0,0 +1,28 @@
+#This is my R script. I am going to use it for data analysis
+
+#install.packages("ggplot2")
+
+library(ggplot2)
+library(tidyr)
+library(collections)
+
+
+
+x<- list(1,2,3)
+x
+x1=list(1,2,3)
+x1
+
+append(x,4)
+x
+
+
+my_function <- function() { # create a function with the name my_function
+  print("Hello World!")
+}
+
+my_function()
+
+
+data<-read.csv("C:/Users/rodge/Downloads/2019 Data.csv")
+data
