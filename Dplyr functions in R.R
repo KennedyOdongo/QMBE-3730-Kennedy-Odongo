5e5192644a650e5219b11b83b1011b675db53665
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-20 11:29:06 -0600

    adding R code

--- Dplyr functions in R.R
+++ Dplyr functions in R.R
@@ -9,3 +9,16 @@ ls(mobile_phone_prices)
 
 #dimensions of the data set -rows, columns
 dim(mobile_phone_prices) ## 2000 rows, 21 columns
+
+## Check data types in R columns
+# Sapply takes in a list as a parameter, applies the function elementwise to that list  and returns its output
+sapply(mobile_phone_prices, class)
+
+#Missing values in R
+is.na(mobile_phone_prices)
+
+#Total number of missing values in R
+sum(is.na(mobile_phone_prices))
+
+#Location of missing values in R
+which(is.na(mobile_phone_prices))
