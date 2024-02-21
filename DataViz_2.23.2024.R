2b0299a5e9dab87b3d6a2a8c12fe811e799ed99a
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-21 16:00:37 -0600

    adding R code and Rmarkdown

--- DataViz_2.23.2024.R
+++ DataViz_2.23.2024.R
@@ -10,6 +10,24 @@ mobile_phone_prices<-read.csv("C:/Users/rodge/OneDrive/Desktop/QMBE 3730 Kennedy
 #plot(column)
 #plot(x, y)
 
+#main() argument
+#The main title is added using the main option in the plot function. 
+#The font, color, and size can be customized using the font.main, col.main and cex.main respectively.
+
+#xlab(), ylab() argument
+#The titles for the axes are provided using xlab and ylab attributes.
+#These can be customized using font.lab, col.lab and cex.lab .
+
+
+#You can also add some extra text inside the plot using the text attribute, 
+#specifying the text to use and the coordinates to display.
+#The text attribute can also be used to label the data points.
+#The text, in this case, is a vector of labels instead of a string.
+
+#legend()
+#The legend can be added to a graph using the R’s legend() function.
+#Legend takes as input the coordinates, text and the symbols to be interpreted.
+
 
 plot(mobile_phone_prices$px_height)
 # type argument allows you to change the type of graph: scatter, line plot
@@ -32,8 +50,6 @@ par(mfrow=c(1,2)) #1 row, 2 columns
 plot(mobile_phone_prices$px_height,mobile_phone_prices$px_width,type='l')
 plot(mobile_phone_prices$px_height,mobile_phone_prices$px_width,pch=c(4,5,6),col=c('red','blue','violet','green'))
 
-
-ls(mobile_phone_prices)
 
 
 library(ggplot2)
