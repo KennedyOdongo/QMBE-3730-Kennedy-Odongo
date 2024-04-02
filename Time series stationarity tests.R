288cea00694adaecb2a5ee88356d47dbad65c0f4
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-04-02 09:27:33 -0500

    adding data

--- Time series stationarity tests.R
+++ Time series stationarity tests.R
@@ -2,10 +2,16 @@ library(tseries)
 data<-read.csv("C:/Users/rodge/Downloads/daily-minimum-temperatures.csv")
 
 
+data_ts<- as.numeric(data$daily_minimum_tempretaures)
 
-plot(data$daily_minimum_tempretaures, type="l")
+diff_1_data <- diff(data_ts, differences = 1)
+diff_1_data
 
+plot(data_ts, type="l")
 
-diff_1_data <- diff(data$daily_minimum_tempretaures, differences = 1)
+plot(diff_1_data, type="l")
+
 
 kpss.test(data$daily_minimum_tempretaures, null="Trend")
+
+(data$daily_minimum_tempretaures)
